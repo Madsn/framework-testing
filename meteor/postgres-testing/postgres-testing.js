42779b722d9e90bf18988519a2a80b9357e87fc3
Madsn <m@madsn.net>
2015-07-26 17:21:10 +0200

    Small test with one collection

--- meteor/postgres-testing/postgres-testing.js
+++ meteor/postgres-testing/postgres-testing.js
@@ -1,22 +1,30 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+tasks = new SQL.Collection('tasks');
 
+if (Meteor.isClient) {
+  var taskTable = {id: ['$number'], text: ['$string'], checked: ["$bool", {$default: false}]};
+  tasks.createTable(taskTable);
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    tasks: function() {
+      return tasks.select().fetch();
     }
   });
-
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    'click #addTask': function(event) {
+      var task = tasks.insert({
+        text:'testing',
+        checked:false
+      })
+      .save();
     }
   });
 }
 
 if (Meteor.isServer) {
+  var taskTable = {text: ['$string'], checked: ["$bool", {$default: false}]};
+  tasks.createTable(taskTable).save();
+  tasks.publish('tasks', function(){
+    return tasks.select();
+  });
   Meteor.startup(function () {
     // code to run on server at startup
   });
