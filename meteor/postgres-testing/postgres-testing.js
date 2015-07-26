61732c28588983d08c052175228362d277ac621e
Madsn <m@madsn.net>
2015-07-26 18:06:54 +0200

    Using autoform for insertions with pg

--- meteor/postgres-testing/postgres-testing.js
+++ meteor/postgres-testing/postgres-testing.js
@@ -1,20 +1,44 @@
 tasks = new SQL.Collection('tasks');
 
+Schema = {};
+Schema.addTaskFormSchema = new SimpleSchema({
+  text: {
+    type: String,
+    label: 'Task text',
+    optional: false,
+    max: 50
+  },
+  checked: {
+    type: Boolean,
+    label: 'Done?'
+  }
+});
+
 if (Meteor.isClient) {
   var taskTable = {id: ['$number'], text: ['$string'], checked: ["$bool", {$default: false}]};
   tasks.createTable(taskTable);
   Template.hello.helpers({
     tasks: function() {
       return tasks.select().fetch();
+    },
+    getCssClass: function(task) {
+      return task.checked ? 'checked': '';
     }
   });
   Template.hello.events({
-    'click #addTask': function(event) {
-      var task = tasks.insert({
-        text:'testing',
-        checked:false
+    'click #deleteAll': function(event) {
+      tasks.remove().save();
+    },
+    'click .toggleDone': function(event) {
+      tasks.update({
+        checked: event.target.checked
       })
-      .save();
+      .where('id = ?', event.target.id).save();
+    }
+  });
+  Template.addTaskForm.helpers({
+    addTaskFormSchema: function() {
+      return Schema.addTaskFormSchema;
     }
   });
 }
@@ -29,3 +53,10 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Meteor.methods({
+  addTask: function(doc) {
+    check(doc, Schema.addTaskFormSchema);
+    tasks.insert(doc).save();
+  }
+});
